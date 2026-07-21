ff77e4680da65eb566053de24f8907faaa27e808
yanggaofeng (C) <y60124509@china.huawei.com>
2026-07-21 21:08:54 +0800

    fix conf_en.py formatting

--- docs/conf_en.py
+++ docs/conf_en.py
@@ -17,7 +17,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-
 """
 English documentation build configuration for Triton Ascend.
 
@@ -111,10 +110,12 @@ import sphinx.util.inspect  # noqa: E402
 
 
 def _unwrap_jit(fn):
+
     def wrapper(obj, **kwargs):
         if isinstance(obj, triton.runtime.JITFunction):
             obj = obj.fn
         return fn(obj, **kwargs)
+
     return wrapper
 
 
